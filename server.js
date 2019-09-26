f915705a222e91d9e541e6819c1cf657901738ad
Glitch (env-setup) <none>
2019-09-26 09:17:59 +0000

    🍇👖 Updated with Glitch

--- server.js
+++ server.js
@@ -1,126 +1,76 @@
 'use strict';
 
 const express     = require('express');
+const cors = require('cors');
+const session     = require('express-session');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
+const auth        = require('./app/auth.js');
+const routes      = require('./app/routes.js');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
-
-const app = express();
+const cookieParser= require('cookie-parser')
+const app         = express();
+app.use(cors());
+const http        = require('http').Server(app);
+const sessionStore= new session.MemoryStore();
+const io          = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
 
 fccTesting(app); //For FCC testing purposes
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'pug')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  key: 'express.sid',
+  store: sessionStore,
+}));
+
+
 mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-        console.log('Successful database connection');
+    if(err) console.log('Database error: ' + err);
+  
+    auth(app, db);
+    routes(app, db);
       
-        app.use(session({
-          secret: process.env.SESSION_SECRET,
-          resave: true,
-          saveUninitialized: true,
-        }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-      
-        function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
-        };
+    http.listen(process.env.PORT || 3000);
 
-        passport.serializeUser((user, done) => {
-          done(null, user.id);
-        });
+  
+    io.use(passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key:          'express.sid',
+      secret:       process.env.SESSION_SECRET,
+      store:        sessionStore
+    }));
+    //start socket.io code  
 
-        passport.deserializeUser((id, done) => {
-            db.collection('socialusers').findOne(
-                {id: id},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
-        });
-
+    var currentUsers = 0;
+    io.on('connection', socket => {
+      console.log('A user has connected');
       
-        /*
-        *  ADD YOUR CODE BELOW
-        */
-        passport.use(new GitHubStrategy({
-          clientID: process.env.GITHUB_CLIENT_ID,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET,
-          callbackURL: 'https://advanced-nodexpress.glitch.me/auth/github/callback'
-        },
-        function(accessToken, refreshToken, profile, cb) {
-            console.log(profile);
-            //Database logic here with callback containing our user object
-            db.db('test').collection('socialusers').findAndModify(
-              {id: profile.id}, {}, {$setOnInsert:{
-                id: profile.id,
-                name: profile.displayName || 'John Doe',
-                photo: profile.photos[0].value || '',
-                email: profile.emails[0].value || 'No public email',
-                created_on: new Date(),
-                provider: profile.provider || ''
-              },
-              $set:{
-                last_login: new Date()
-              },
-              $inc:{
-                login_count: 1
-              }},
-              {upsert:true, new: true}, (err, doc) => { 
-                return cb(null, doc.value);
-              }
-            );
-        }));
+      ++currentUsers;
+      console.log('user ' + socket.request.user.name + ' connected');
+      io.emit('user', {name: socket.request.user.name, currentUsers, connected: true});
       
-        app.route('/auth/github')
-          .get(passport.authenticate('github'));
-
-        app.route('/auth/github/callback')
-          .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
-          res.redirect('/profile');
-        });
+      socket.on('chat message', (message) => {
+        io.emit('chat message', {name: socket.request.user.name, message});
+      });
       
-        /*
-        *  ADD YOUR CODE ABOVE
-        */
-      
-      
-        app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index');
-          });
-
-        app.route('/profile')
-          .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-          });
-
-        app.route('/logout')
-          .get((req, res) => {
-              req.logout();
-              res.redirect('/');
-          });
-
-        app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
-        });
-      
-        app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+      /* --currentUsers;
+        io.emit('disconnect', currentUsers);*/
+      socket.on('disconnect', (user) => {
+        --currentUsers;
+        io.emit('disconnect', user);
+        console.log( socket.request.user.name + ' has left the chat.');
+      });
+    });
+    
+    //end socket.io code
+});
